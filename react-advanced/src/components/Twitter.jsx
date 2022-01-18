314a11d05e91c0b00df1fafbab6e237213c34212
marcusWittho <mcswitthoeft@gmail.com>
2022-01-17 22:44:14 -0300

    Refatoração do arquivo Twitter.jsx para componente funcional

--- react-advanced/src/components/Twitter.jsx
+++ react-advanced/src/components/Twitter.jsx
@@ -1,36 +1,47 @@
 import React from 'react';
 
-class Twitter extends React.Component {
-
-  state = {
-    tweet: 'tweet',
-  };
-
-  componentDidMount() {
-    const { posts, loading } = this.props;
-    console.log('DidMount', posts);
-    console.log('DidMount', loading);
-  };
-
-  componentDidUpdate(prevProps) {
-    const { loading } = this.props;
-
-    if(this.props.loading !== prevProps.loading) {
-      console.log('DidUpdate: loading', loading);
-    }
-  };
-
-  componentWillUnmount() {
-    console.log('WillUnmount: fui desmontado...');
-  };
-
-  render() {
-    return (
-      <>
-        <h3>Twitter</h3>
-      </>
-    )
-  }
+// Este hook equivale ao 'shouldComponentUpdate'
+// Evita re-render
+// Utiliza o 'React.memo'
+const areEqual = (prevProps, nextProps) => {
+  return prevProps.loading === nextProps.loading;
+  // return true;  // se o retorno for 'true' a atualização não é realizada
 };
 
-export default Twitter;
+function Twitter(props) {
+  const { posts, loading } = props;
+  const [tweet, setTweet] = React.useState('title');
+
+  // Este hook tem o mesmo efeito do 'componentDidMount'
+  React.useEffect(() => {
+    console.log('DidMount', posts);
+    console.log('DidMount', loading);
+  }, [])
+
+  // Este hook tem o comportamento do 'componentDidUpdate'
+  React.useEffect(() => {
+    console.log('DidUpdate', loading);
+  }, [loading]);
+
+  // Este hook tem o efeito do 'componentWillUnmount'
+  // O retorno dentro do useEffect caracteriza o funcionamento do componentWillUnmunt
+  React.useEffect(() => {
+    return () => {
+      console.log('WillUnmount: fui desmontado...');
+    }
+  }, []);
+
+  const handleTweet = () => {
+    setTweet('Tweet atualizado. (handleTweet)')
+  };
+
+  console.log('Tweet atualizado:', tweet);
+  return (
+    <>
+      <button onClick={ handleTweet }>Re-render</button>
+      <h3>Twitter</h3>
+    </>
+  )
+};
+
+export default React.memo(Twitter, areEqual);
