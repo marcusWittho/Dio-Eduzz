85c41872eadd832e962228c045a7e2ae37847e98
marcusWittho <mcswitthoeft@gmail.com>
2022-01-19 17:02:05 -0300

    Configuração e exemplo de uso do Error Boundary

---
+++ react-components-dom/src/App.js
@@ -0,0 +1,40 @@
+import React, { Fragment } from 'react';
+
+const store = [
+  {
+    type: 'Roupa'
+  },
+  {
+    type: 'Calçado'
+  },
+  {
+    type: 'Camiseta'
+  }
+]
+
+function Column({ type }) {
+  return (
+    <tr>
+      <td>{ type }</td>
+    </tr>
+  )
+};
+
+function App() {
+  const renderColumns = (element, key) => (
+    <Fragment key={ `column-${key}` }>
+      <Column type={ element.type.undefined.error } />
+      {/* <Column type={ element.type } /> */}
+    </Fragment>
+  )
+
+  return (
+    <table>
+      <tbody>
+        {store.map(renderColumns)}
+      </tbody>
+    </table>
+  );
+}
+
+export default App;
---
+++ react-components-dom/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import ErrorBoundary from './ErrorBoundary';
+
+const main = (
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>
+)
+
+ReactDOM.render(
+  main,
+  document.getElementById('root')
+);
